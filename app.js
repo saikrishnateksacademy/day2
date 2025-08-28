c5b70cd4a945667d0f3cfc6dc6c0d13e544dcbeb
saikrishnateksacademy <Saikrishna.Ennam@teksacademy.com>
2025-08-28 18:24:21 +0530

    Updated feature X / fixed bug Y

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const port = 3000;
 const app = express()
 
 const router = require('./routes.js/index');
-app.use('/',router)
+app.use('/',router,(req,res)=>{
+res.send("this is the home page type the end point ant the url to see the output of routers /api/v1/users  /api/v1/admin/users /api/v1/admin/users ")
+})
 
 app.listen(port, ()=>{
     console.log(`server is running at http://localhost:${port}`)
